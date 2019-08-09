2891eaa43b4bc5c83845eafcfe42cb13826ac008
Filipe Pereira <filipejfpereira@gmail.com>
2019-08-09 04:13:34 +0100

    database working

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
     console.log(event)
+
+    fetchRoles();
+    $(document).on('click','#add-event-btn', function(){
+        
+        addEvent();
+    })
+
     $(document).on('click','#add-role-btn', function(){
         
         addRole();
     })
+
     $(document).on('click', '#add-item-btn', function(){
         addItem();
     })
@@ -11,12 +19,91 @@ $(document).ready(function(){
     
 });
 
+ function fetchRoles(){
+    $.ajax({
+        url: 'http://192.168.248.200:3000/roles',
+        type: 'GET',
+        data: JSON.parse(response),
+        async: true,
+        sucess: function(data){ 
+            populateTable(data);
+        },
+        error: function(){
+            console.log('you are dumb');
+        }
+
+    })
+ }
+
+function populateTable(data){
+
+    var itemtable = $('#role-table');
+    var row;
+    data.forEach(function(element){
+
+        alert("hey")
+         row =  "<tr>" + 
+         "<td>" + element.name +"</td>" +
+         "<td>" + element.user + "</td>" +
+        "</tr>";
+
+        $(row).appendTo(itemtable);
+    })
+}
+
+function addEvent(){
+    var event = {
+        name: $('#eventName').val(),
+        place: $('#eventLocation').val(),
+        guests: $('#guestsnumber').val()
+    }
+
+    $.ajax({
+        url: 'http://192.168.248.200:3000/events',
+        type: 'POST',
+        data: JSON.stringify(event),
+        async: true,
+        contentType: 'application/json',
+        sucess: function(){ window.location.reload();
+        },
+        error: function(){
+            console.log('you are dumb');
+        }
+
+    })
+
+
+}
+
+
+
+
+
 function addRole(){
+
+    var role = {
+        event: $('#eventName').val(),
+        name: $('#roleNameInput').val(),
+        user: $('#userNameInput').val()
+    }
+    $.ajax({
+        url: 'http://192.168.248.200:3000/roles',
+        type: 'POST',
+        data: JSON.stringify(role),
+        async: true,
+        contentType: 'application/json',
+        sucess: function(){ fetchRoles();
+        },
+        error: function(){
+            console.log('you are dumb');
+        }
+
+    })
+}
     
-        var name = $('#userNameInput').val();
+    /*var name = $('#userNameInput').val();
         var roleValue = $('#roleNameInput').val();
-        /*userArray.forEach(element => {
-        if( name === element.id ){|*/
+
           var rolestable = $('#role-table');
     var row;
     
@@ -49,4 +136,4 @@ function addRole(){
      
             $(row).appendTo(itemtable);
 
-        }
+    }*/
